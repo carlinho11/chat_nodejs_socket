b09fd9be795cdd25ad2a4f017835472baebd5c75
carlinho11 <38302156+carlinho11@users.noreply.github.com>
2021-09-02 10:24:19 -0300

    Ok

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ var app = express();
 
 app.use(cors())
 
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
 const server = require('http').createServer(app);
 
 const io = require('socket.io')(server);
 
+console.log(process.env.PORT)
 
 server.listen(process.env.PORT || 3000, () =>{
   console.log('Conectado');
